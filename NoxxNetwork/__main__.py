e774b8be6eeffba9ce7c1dda84f820b0a9eff51f
gaganhero89-blip <gaganhero89@gmail.com>
2026-04-24 21:32:35 +0530

    Update __main__.py

--- NoxxNetwork/__main__.py
+++ NoxxNetwork/__main__.py
@@ -1,26 +1,36 @@
 import asyncio
 import importlib
+import os
+from threading import Thread
+from flask import Flask
 
 from pyrogram import idle
-
 from NoxxNetwork import LOGGER, NoxxBot
 from NoxxNetwork.modules import ALL_MODULES
 
+# fake web server
+app = Flask(__name__)
 
-async def anony_boot():
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run_server():
+    app.run(host="0.0.0.0", port=8080)
+
+async def bot_start():
     try:
         await NoxxBot.start()
     except Exception as ex:
-        LOGGER.error(ex)
-        quit(1)
+        LOGGER.error(f"Failed to start bot: {ex}")
+        return
 
-    for all_module in ALL_MODULES:
-        importlib.import_module("NoxxNetwork.modules." + all_module)
+    for module in ALL_MODULES:
+        importlib.import_module("NoxxNetwork.modules." + module)
 
-    LOGGER.info(f"@{NoxxBot.username} Started.")
+    LOGGER.info(f"@{NoxxBot.username} Started Successfully.")
     await idle()
 
-
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(anony_boot())
-    LOGGER.info("Stopping NoxxNetwork Bot...")
+    Thread(target=run_server, daemon=True).start()
+    asyncio.run(bot_start())
